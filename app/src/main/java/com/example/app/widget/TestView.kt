0b19d68269b200d9a7d0e370a7cf0aa6931e866b
sungen <sungentim@gmail.com>
2020-06-06 07:44:40 +0800

    rengwuxian custom view

--- app/src/main/java/com/example/app/widget/TestView.kt
+++ app/src/main/java/com/example/app/widget/TestView.kt
@@ -7,9 +7,8 @@ import android.util.AttributeSet
 import android.view.View
 import com.example.app.dp
 
-val RADIUS = 100f.dp
-
 class TestView(context: Context, attributes: AttributeSet) : View(context, attributes) {
+    val RADIUS = 100f.dp
     override fun onDraw(canvas: Canvas) {
         var paint = Paint(Paint.ANTI_ALIAS_FLAG)
         canvas.drawLine(100f, 100f, 200f, 200f, paint)
